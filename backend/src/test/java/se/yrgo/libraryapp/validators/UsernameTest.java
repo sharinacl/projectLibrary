cafd558272e5580277c13e4dfac33eb01ce7f47d
Sharina <shacha.dev@gmail.com>
2025-11-21 09:46:52 +0100

    task one UsernameTest

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,17 +1,22 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class UsernameTest {
     @Test
     void correctUsername() {
-        assertTrue(Username.validate("bosse"));
+//        assertTrue(Username.validate("bosse"));
+        boolean resultTrue = Username.validate("bosse");
+        assertThat(resultTrue).isTrue();
     }
 
     @Test
     void incorrectUsername() {
-        assertFalse(Username.validate("name with space"));
+//        assertFalse(Username.validate("name with space"));
+        boolean resultFalse = Username.validate("name with space");
+        assertThat(resultFalse).isFalse();
     }
+
 }
