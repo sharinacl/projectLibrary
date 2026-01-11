35e888e9b5d9f639c993fa81cf5c10914cfa752e
Sharina <shacha.dev@gmail.com>
2026-01-11 20:47:28 +0100

    fix(username): correct regex to match allowed character set

--- backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
+++ backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
  * I.e. no funny characters or whitespace and at least four characters long.
  */
 public final class Username {
-    private static Pattern regex = Pattern.compile("[@._a-zA-Z0-]{4,}");
+    private static Pattern regex = Pattern.compile("[@._a-zA-Z0-9-]{4,}");
 
     private Username() {}
 
