58e231d3e00613e97cd357a7a1e1390c9b7831d3
Sharina <shacha.dev@gmail.com>
2026-01-31 23:05:17 +0100

    Add H2/FlyWay integration test for UserDao

---
+++ backend/src/test/java/se/yrgo/libraryapp/dao/UserDaoIntegrationTest.java
@@ -0,0 +1,43 @@
+package se.yrgo.libraryapp.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.radcortez.flyway.test.annotation.H2;
+
+import se.yrgo.libraryapp.entities.User;
+import se.yrgo.libraryapp.entities.UserId;
+
+@Tag("integration")
+@H2
+public class UserDaoIntegrationTest {
+    private static DataSource ds;
+
+    @BeforeAll
+    static void initDataSource() {
+        final JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:mem:test");
+        UserDaoIntegrationTest.ds = ds;
+    }
+
+    @Test
+    void getUserById() {
+        final String username = "test";
+        final UserId userId = UserId.of(1);
+
+        UserDao userDao = new UserDao(ds);
+        Optional<User> maybeUser = userDao.get(Integer.toString(userId.getId()));
+
+        assertThat(maybeUser).isPresent();
+        assertThat(maybeUser.get().getName()).isEqualTo(username);
+        assertThat(maybeUser.get().getId()).isEqualTo(userId);
+    }
+}
